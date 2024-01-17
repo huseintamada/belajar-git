6959d624fce100c35a9f8047e79e6b1b4161bbcb
huseintamada <ahmadhuseintamada@gmail.com>
2024-01-17 09:45:00 +0700

    menambahkan baris input jurusan

--- biodata.py
+++ biodata.py
@@ -1,7 +1,10 @@
 nama = input("Masukkan nama: ")
 umur = input("Masukkan umur: ")
 alamat = input("Masukkan alamat: ")
+Jurusan= input("Masukkan Jurusan: ")
+
 print("\n-------Biodata-------")
 print("Nama: " + nama)
 print("Umur: " + umur)
 print("Alamat: " + alamat)
+print("Jurusan: " + Jurusan)
