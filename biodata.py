f967048cd4ad70eda0f89d02a749bfe70704d72a
Tamada <155598617+huseintamada@users.noreply.github.com>
2024-01-17 09:28:09 +0700

    Membuat biodata.py
    kode biodata

---
+++ biodata.py
@@ -0,0 +1,7 @@
+nama = input("Masukkan nama: ")
+umur = input("Masukkan umur: ")
+alamat = input("Masukkan alamat: ")
+print("\n-------Biodata-------")
+print("Nama: " + nama)
+print("Umur: " + umur)
+print("Alamat: " + alamat)
